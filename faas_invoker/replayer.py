9e47687c2baf0cf4e7aef2cc2c120f2677933000
zhaojie <8432624+wen-zhaojie@user.noreply.gitee.com>
2023-01-11 23:04:16 +0800

    Update for replayer test

--- faas_invoker/replayer.py
+++ faas_invoker/replayer.py
@@ -2,6 +2,8 @@ import time
 import copy
 from knative import KnativeInvoker
 from multiprocessing import Manager
+import multiprocessing as mp
+import json
 
 
 class Replayer:
@@ -9,8 +11,39 @@ class Replayer:
         self.invoker = invoker
         pass
 
-    def trace_replayer(self, invocation_in_sec_list):
-        pass
+    def trace_replayer(self, namespace, function_name, handler, invocation_in_sec_list):
+        m = Manager()
+        res_queue = m.Queue()
+
+        process_list = []
+
+        for timestamp in range(10):
+            start_time = time.time()
+            print(f"当前模拟调用第{timestamp}秒")
+            n_request = 3
+            p = mp.Process(target=self.invoke_in_sec,
+                                        args=(res_queue, namespace, function_name, handler, timestamp, n_request))
+            process_list.append(p)
+            p.start()
+            end_time = time.time()
+            send_time = end_time - start_time
+            print(f"休息{1 - send_time}秒")
+            time.sleep(1 - send_time)
+
+        for process in process_list:
+            process.join()
+
+        # 展示结果保存
+        print("从缓存队列中取结果!")
+        result_dict_list = []
+        while not res_queue.empty():
+            res = res_queue.get()
+            print(f"type(res):{type(res)}")
+            res = [json.loads(item) for item in res]
+            result_dict_list.extend(res)
+
+        print(f"log_dict_list:{result_dict_list}")
+
 
     def invoke_in_sec(self, res_queue, namespace="faas-scaler", function_name="test-intra-parallelism", handler="matmul", timestamp=0, n_request=1):
         input_obj = {
@@ -51,9 +84,14 @@ class Replayer:
 
         print(f"log_dict_list:{log_dict_list}")
 
+    def test_trace_replayer(self):
+        self.trace_replayer(namespace="faas-scaler", function_name="test-intra-parallelism", handler="matmul", invocation_in_sec_list=[1,0,0,5,0,0,0,1])
+
 
 if __name__ == "__main__":
     replayer = Replayer()
     replayer.test_invoke_in_sec()
+
+    # 测试 test_invoke_in_sec
 
 
